1d01557e1e9aafd443cdd4818f13cc202e7dfbda
Tamanna Chadni <tamanna.chadni.eng@gmail.com>
2024-04-30 00:33:40 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,145 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASS);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.avbafwc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const touristCollection = client.db("touristSpot").collection("spot");
+    const userCollection = client.db("touristSpot").collection("user");
+    const countryCollection = client.db("countryName").collection("country");
+
+    // country api
+
+    app.get("/country", async (req, res) => {
+      const cursor = countryCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // spot api
+
+    app.get("/spot", async (req, res) => {
+      const cursor = touristCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/spot/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await touristCollection.findOne(query);
+      res.send(result);
+    });
+    app.put("/spot/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedSpot = req.body;
+
+      const spot = {
+        $set: {
+          name: updatedSpot.name,
+          quantity: updatedSpot.quantity,
+          supplier: updatedSpot.supplier,
+          taste: updatedSpot.taste,
+          category: updatedSpot.category,
+          details: updatedSpot.details,
+          photo: updatedSpot.photo,
+        },
+      };
+
+      const result = await touristCollection.updateOne(filter, spot, options);
+      res.send(result);
+    });
+
+    app.post("/spot", async (req, res) => {
+      const newSpot = req.body;
+      console.log(newSpot);
+      const result = await touristCollection.insertOne(newSpot);
+      res.send(result);
+    });
+
+    app.delete("/spot/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await touristCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // user related apis
+    app.get("/user", async (req, res) => {
+      const cursor = userCollection.find();
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch("/user", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = {
+        $set: {
+          lastLoggedAt: user.lastLoggedAt,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("tourist spot server is running");
+});
+
+app.listen(port, () => {
+  console.log(`spot: ${port}`);
+});
+
+

11a420d79ecb7f00520409efefc96b5b1295b89b
Tamanna Chadni <tamanna.chadni.eng@gmail.com>
2024-04-30 16:50:56 +0600

    deploy in vercel

--- index.js
+++ index.js
@@ -125,22 +125,22 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
-run().catch(console.dir);
+// run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("tourist spot server is running");
+  // res.send("tourist spot server is running");
 });
 
 app.listen(port, () => {
-  console.log(`spot: ${port}`);
+  // console.log(`spot: ${port}`);
 });
 
 
